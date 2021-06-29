bb7f82db0ba9a3d4bad304c4ba44da3318420903
Istiaque Khan <Istiaque.Khan49@myhunter.cuny.edu>
2021-06-28 20:50:22 -0400

    Added home, login, userprofile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+    
+    class Home extends Component {
+      render() {
+        return (
+            <div>
+              <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+              <h1>Bank of React</h1>
+            </div>
+        );
+      }
+    }
 
 export default App;
